733fe17ef7a614073458b3d1a3bb1673d0f32def
CinyLiang <ciny.liang@mail.utoronto.ca>
2025-11-13 13:28:31 -0500

    Task 2.1

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -40,9 +40,6 @@ public class StatementPrinter {
 
         for (Performance p : invoice.getPerformances()) {
 
-            int thisAmount = 0;
-            thisAmount = getAmount(p);
-
             // add volume credits
             volumeCredits += Math.max(
                     p.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
@@ -54,9 +51,9 @@ public class StatementPrinter {
             // print line for this order
             result.append(String.format("  %s: %s (%s seats)%n",
                     getPlay(p).getName(),
-                    frmt.format(thisAmount / Constants.PERCENT_FACTOR),
+                    frmt.format(getAmount(p) / Constants.PERCENT_FACTOR),
                     p.getAudience()));
-            totalAmount += thisAmount;
+            totalAmount += getAmount(p);
         }
         result.append(String.format("Amount owed is %s%n", frmt.format(
                 totalAmount / Constants.PERCENT_FACTOR)));
